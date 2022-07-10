1a7fe38e6dc4645925a87b1bc7a5467c0111c1e8
Dominik <d.jasinski@ideo.pl>
2022-07-10 20:45:37 +0200

    Set Status Notify All Voters

--- resources/views/livewire/set-status.blade.php
+++ resources/views/livewire/set-status.blade.php
@@ -72,14 +72,14 @@
                 </button>
                 <button
                     type="submit"
-                    class="flex items-center justify-center w-1/2 h-9 text-xs bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3 text-white"
+                    class="flex items-center justify-center w-1/2 h-9 text-xs bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3 text-white disabled:opacity-50"
                 >
-                    <span>Update</span>
+                    Update
                 </button>
             </div>
             <div>
                 <label class="font-normal inline-flex items-center">
-                    <input type="checkbox" name="notify_voters" class="rounded bg-gray-200" checked>
+                    <input wire:model="notifyAllVoters" type="checkbox" name="notify_voters" class="rounded bg-gray-200">
                     <span class="ml-2">Notify all voters</span>
                 </label>
             </div>
