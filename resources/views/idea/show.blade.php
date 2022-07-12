afbdf06508de2cb5fd84ffbe804df6a86b3ff001
Dominik <d.jasinski@ideo.pl>
2022-07-12 09:33:23 +0200

    Refactor Modals to Blade Component

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -13,17 +13,7 @@
         :votesCount="$votesCount"
     />
 
-    @can('update', $idea)
-        <livewire:edit-idea :idea="$idea" />
-    @endcan
-
-    @can('delete', $idea)
-        <livewire:delete-idea :idea="$idea" />
-    @endcan
-
-    <livewire:mark-idea-as-spam :idea="$idea" />
-
-    <livewire:mark-idea-as-not-spam :idea="$idea" />
+    <x-modals-container :idea="$idea" />
 
     <div class="comments-container relative space-y-6 md:ml-22 my-8 mt-1 pt-4">
         <div class="comment-container relative bg-white rounded-xl flex cursor-pointer mt-4">
