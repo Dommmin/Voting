558b09bea1aea980f20f3c9d9e65c81c1c11a85d
Dominik <d.jasinski@ideo.pl>
2022-07-16 17:26:00 +0200

    Comment Migration and Models

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Idea extends Model
 {
@@ -17,6 +18,11 @@ class Idea extends Model
     const PAGINATION_COUNT = 10;
 
     protected $guarded = [];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 
     public function user(): BelongsTo
     {
