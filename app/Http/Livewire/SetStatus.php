a0119fc4a4323095a5f6fccba65f7dca2b97dd89
Dominik <d.jasinski@ideo.pl>
2022-07-17 17:51:54 +0200

    Admin set status comment

--- app/Http/Livewire/SetStatus.php
+++ app/Http/Livewire/SetStatus.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Jobs\NotifyAllVoters;
+use App\Models\Comment;
 use App\Models\Idea;
 use Livewire\Component;
 
@@ -11,6 +12,7 @@ class SetStatus extends Component
     public $idea;
     public $status;
     public $notifyAllVoters;
+    public $comment;
 
     public function mount(Idea $idea)
     {
@@ -30,7 +32,15 @@ class SetStatus extends Component
             NotifyAllVoters::dispatch($this->idea);
         }
 
-        $this->emit('statusWasUpdated');
+        Comment::create([
+            'user_id' => auth()->id(),
+            'idea_id' => $this->idea->id,
+            'status_id' => $this->status,
+            'body' => $this->comment ?? 'No comment was added.',
+            'is_status_update' => true,
+        ]);
+
+        $this->emit('statusWasUpdated', 'Status was updated successfully!');
     }
 
     public function render()
--- resources/views/livewire/set-status.blade.php
+++ resources/views/livewire/set-status.blade.php
@@ -55,7 +55,7 @@
             </div>
 
             <div>
-                <textarea name="update_comment" id="update_comments" cols="30" rows="4"
+                <textarea wire:model="comment" name="update_comment" id="update_comments" cols="30" rows="4"
                           class="w-full text-sm bg-gray-100 rounded-xl placeholder-gray-900 border-none px-4 py-2"
                           placeholder="Add an update comment (optional)"
                 ></textarea>
