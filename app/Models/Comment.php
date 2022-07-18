c418ecc9b96ccaa2ccbc1b4a6800f22665c035c4
Dominik <d.jasinski@ideo.pl>
2022-07-18 21:29:54 +0200

    Mark Notification as Read

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -12,7 +12,7 @@ class Comment extends Model
     use HasFactory;
 
     protected $guarded = [];
-    protected $perPage = 25;
+    protected $perPage = 20;
 
 
     public function user()
--- resources/views/livewire/comment-notifications.blade.php
+++ resources/views/livewire/comment-notifications.blade.php
@@ -26,9 +26,10 @@
                 <li>
                     <a
                         href="{{ route('idea.show', $notification->data['idea_slug']) }}"
-                        {{--                    @click.prevent="--}}
-                        {{--                        isOpen = false--}}
-                        {{--                    "--}}
+                        @click.prevent="
+                            isOpen = false
+                        "
+                        wire:click.prevent="markAsRead('{{ $notification->id }}')"
                         class="flex hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in"
                     >
                         <img src="{{ $notification->data['user_avatar'] }}" class="rounded-xl w-10 h-10" alt="avatar">
@@ -48,6 +49,8 @@
 
         <li class="border-t border-gray-300 text-center">
             <button
+                wire:click="markAllAsRead"
+                @click="isOpen = false"
                 class="w-full block font-semibold hover:bg-gray-100 px-5 py-3 transition duration-150 ease-in py-4"
             >
                 Mark all as read
